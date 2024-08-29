b72accf3c3cd94363dfa89c1a961fe7915b1f4e7
AcevedoAdrian <acevedohugoadrian@gmail.com>
2024-08-29 13:02:21 -0300

    feat: Update header background color to indigo in App component

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
 	return (
 		<>
-			<header className='bg-blue-600 py-8 max-h-72'>
+			<header className='bg-indigo-700 py-8 max-h-72'>
 				<h1 className='uppercase text-center font-black text-4xl text-white '>
 					Control de Gastos
 				</h1>
@@ -26,5 +26,6 @@ function App() {
 		</>
 	);
 }
+
 
 export default App;
--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,15 +1,50 @@
+import { useState, ChangeEvent, FormEvent } from 'react';
 import { categories } from '../data/categories';
 import DatePicker from 'react-date-picker';
+import type { DraftExpense, Value } from '../types';
 import 'react-date-picker/dist/DatePicker.css';
 import 'react-calendar/dist/Calendar.css';
 
-type ValuePiece = Date | null;
-
-type Value = ValuePiece | [ValuePiece, ValuePiece];
-
 export default function ExpenseForm() {
+	const [expense, setExpense] = useState<DraftExpense>({
+		amount: 0,
+		expenseName: '',
+		category: '',
+		date: new Date(),
+	});
+	const [error, setError] = useState('');
+
+	const handleChange = (
+		event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+	) => {
+		const { name, value } = event.target;
+		const isAmount = ['amount'].includes(name);
+		setExpense({
+			...expense,
+			[name]: isAmount ? Number(value) : value,
+		});
+	};
+
+	const handleChangeDate = (value: Value) => {
+		setExpense({
+			...expense,
+			date: value,
+		});
+	};
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		// validate inputs
+		if (Object.values(expense).includes('')) {
+			setError('Todos los campos son obligatorios');
+			return;
+		}
+		console.log('Todo correcto');
+	};
 	return (
-		<form className='space-y-5'>
+		<form
+			className='space-y-5'
+			onSubmit={handleSubmit}>
 			<legend className='uppercase text-center font-black text-2xl text-gray-600 border-b-2 border-blue-600 py-2'>
 				Nuevo Gasto
 			</legend>
@@ -25,6 +60,8 @@ export default function ExpenseForm() {
 					placeholder='Agrege el nombre del gasto: Ej. Comida'
 					className='bg-slate-100 p-2'
 					name='expenseName'
+					value={expense.expenseName}
+					onChange={handleChange}
 				/>
 			</div>
 			<div className='flex flex-col'>
@@ -39,6 +76,8 @@ export default function ExpenseForm() {
 					placeholder='Agrege la cantidad del gasto: Ej. 100'
 					className='bg-slate-100 p-2'
 					name='amount'
+					value={expense.amount}
+					onChange={handleChange}
 				/>
 			</div>
 			<div className='flex flex-col'>
@@ -50,7 +89,9 @@ export default function ExpenseForm() {
 				<select
 					name='category'
 					className='bg-slate-100 p-2'
-					id='category'>
+					id='category'
+					value={expense.category}
+					onChange={handleChange}>
 					<option value=''>Seleccione una categoria</option>
 					{
 						/* categories */
@@ -70,7 +111,11 @@ export default function ExpenseForm() {
 					className='text-xl text-gray-800'>
 					Fecha Gasto:
 				</label>
-				<DatePicker className='bg-slate-100 p-2 border-0' />
+				<DatePicker
+					className='bg-slate-100 p-2 border-0'
+					value={expense.date}
+					onChange={handleChangeDate}
+				/>
 			</div>
 			<input
 				type='submit'
